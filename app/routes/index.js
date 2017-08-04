0b752a2c89d34135c5c9466832de8e92c93684db
JoaoVKG <joao99vkg@gmail.com>
2017-08-03 22:49:43 -0300

    console log application

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 module.exports = function (application) {
     application.get('/', function (req, res) {
+        console.log(application.app);
         application.app.controllers.index.home(application, req, res);
     });
 }
