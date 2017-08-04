7403c7b40b1cf493d6ba8a928dd98867f02ccb63
JoaoVKG <joao99vkg@gmail.com>
2017-08-03 23:42:16 -0300

    changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const app = require('./config/server');
+var app = require('./config/server');
 
 const server = app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor está online");
--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ const consign = require('consign');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 
-const app = express();
+var app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
