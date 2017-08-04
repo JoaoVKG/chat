d80fd04808c16004a4432d881cf8da947960099c
JoaoVKG <joao99vkg@gmail.com>
2017-08-03 21:04:37 -0300

    server changed

--- config/server.js
+++ config/server.js
@@ -8,16 +8,18 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
+consign()
+    .include('app/routes')
+    .then('app/models')
+    .then('app/controllers')
+    .into(app);
+
 app.use(express.static('./app/public'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(expressValidator());
 
-consign()
-    .include('app/routes')
-    .then('app/models')
-    .then('app/controllers')
-    .into(app);
+
 
 module.exports = app;
