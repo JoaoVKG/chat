c7bdc4e352b7f0a9c55fedbe051280629d70c275
JoaoVKG <joao99vkg@gmail.com>
2017-08-10 19:55:45 -0300

    new funcionality

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = require('./config/server');
 
-const server = app.listen(process.env.PORT, function() {
-    console.log("Servidor está online na porta " + process.env.PORT);
+const server = app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor está online na porta " + process.env.PORT || 3000);
 })
 
 const io = require('socket.io').listen(server);
@@ -28,14 +28,21 @@ io.on('connection', function(socket) {
     })
 
     socket.on('msgParaServidor', function(data) {
-        socket.emit('msgParaCliente',
-            {apelido: data.apelido, mensagem: data.mensagem }
-        );
+        if(data.mensagem == "gemidao-do-zap.exe") {
+            socket.broadcast.emit('gemidao');
+        } else {
+            socket.emit('msgParaCliente',
+                {apelido: data.apelido, mensagem: data.mensagem }
+            );
 
-        socket.broadcast.emit('msgParaCliente',
-            {apelido: data.apelido}
-        );
+            socket.broadcast.emit('msgParaCliente',
+                {apelido: data.apelido}
+            );
 
+        }
+
+
+        
         if(parseInt(data.apelido_atualizado_nos_clientes) == 0 ) {
             socket.emit('participantesParaCliente',
                 {apelido: data.apelido}
